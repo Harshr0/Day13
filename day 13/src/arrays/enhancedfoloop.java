65ea7bc2529e9c472d4db2209d5d0a5583993578
Jishu <Jishu@DESKTOP-J5EUKD0>
2022-08-12 11:50:55 +0530

    Day 13

---
+++ day 13/src/arrays/enhancedfoloop.java
@@ -0,0 +1,12 @@
+package arrays;
+import java.util.*;
+public class enhancedfoloop {
+
+	public static void main(String[] args) {
+		int [] arr = {12, 4, 5, 2, 5};
+		for (int i : arr){ {
+		System.out.print(i);
+		}}
+	}
+
+}
